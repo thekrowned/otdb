c5934ded38393c3eb531fa9711140057760bfe72
Sheppsu <49356627+Sheppsu@users.noreply.github.com>
2024-09-15 03:38:30 -0400

    append slash on users/

--- otdb/main/urls.py
+++ otdb/main/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("dashboard/", views.dashboard, name="dashboard"),
-    path("users/<int:id>", views.user, name="user"),
+    path("users/<int:id>/", views.user, name="user"),
 ]
